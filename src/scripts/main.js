be75fbb2e5f4dbcec68c5d1c825bc5adabae1458
Dariia Hryshchukova <dariagrisukova@gmail.com>
2025-08-08 14:48:01 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+function ShowMessage(message, isError = false) {
+  const el = document.createElement('div');
+
+  el.classList.add('message');
+
+  if (isError) {
+    el.classList.add('error-message');
+  }
+  el.textContent = message;
+
+  document.body.appendChild(el);
+}
+
+logo.addEventListener('click', (e) => {
+  const click = e.target;
+
+  // eslint-disable-next-line no-new
+  new Promise((resolve, reject) => {
+    if (!click) {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject(new Error('Promise was rejected!'));
+    } else {
+      resolve('Promise was resolved!');
+    }
+  })
+    .then((value) => {
+      ShowMessage(value);
+    })
+    .catch((err) => {
+      ShowMessage(err.message, true);
+    });
+});
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+})
+  .then((value) => {
+    ShowMessage(`${value}`);
+  })
+  .catch((err) => {
+    ShowMessage(err.message, true);
+  });
